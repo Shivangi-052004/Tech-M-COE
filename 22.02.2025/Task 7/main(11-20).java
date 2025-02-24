9c3648d90e9b04d4a1dcddb5481ab75b57a928cc
Shivangi-052004 <shivangidayaaece@gmail.com>
2025-02-24 20:05:59 +0530

    Create main(11-20).java

---
+++ 22.02.2025/Task 7/main(11-20).java
@@ -0,0 +1,18 @@
+package sample;
+
+import java.util.*;
+
+public static void main(String[] args) {
+        createAndPrintColors();
+        iterateColors();
+        insertAtFirstPosition();
+        retrieveElement();
+        updateElement();
+        removeThirdElement();
+        searchElement();
+        sortList();
+        copyList();
+        shuffleList();
+        appendToLinkedList();
+        iterateLinkedList();
+    }
