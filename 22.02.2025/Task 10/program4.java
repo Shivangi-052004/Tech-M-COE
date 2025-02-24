272fb7b6b5e3a8c7653494af2da3a84a9e5a1d33
Shivangi-052004 <shivangidayaaece@gmail.com>
2025-02-24 20:45:11 +0530

    Update program4.java

--- 22.02.2025/Task 10/program4.java
+++ 22.02.2025/Task 10/program4.java
@@ -1,32 +1,21 @@
-package com.example;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DisplayHtmlServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        
-        // Set the content type to HTML
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        
-        // Get the PrintWriter to write the response
         PrintWriter out = response.getWriter();
-        
-        // Display the HTML content
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head><title>HTML from Servlet</title></head>");
-        out.println("<body>");
-        out.println("<h1>Displayed using Java Servlet</h1>");
-        out.println("<p>This content is served from the DisplayHtmlServlet.</p>");
-        out.println("<a href='index.html'>Go to HTML Page</a>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("<html><head><title>Servlet Response</title></head><body>");
+        out.println("<h2>Hello, this is a response from the Servlet!</h2>");
+        out.println("<p>Servlets make dynamic web content easy.</p>");
+        out.println("</body></html>");
     }
 }
